9804b76e3dcd90d30bed52f91be4fb3bb897bd61
Tristan Livengood <tlivengood55@gmail.com>
2023-12-10 04:39:37 -0500

    inputted routes for products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,68 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/products', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [Category, { model: Tag, through: ProductTag }],
+    });
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/products/:id', async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findByPk(productId, {
+      include: [Category, { model: Tag, through: ProductTag }],
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/products', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/products/:id', async (req, res) => {
+  try {
+    const updatedProduct = await Product.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.json(updatedProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// DELETE a product by ID
+router.delete('/products/:id', async (req, res) => {
+  try {
+    await Product.destroy({ where: { id: req.params.id } });
+    res.send('Product deleted');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const { sequelize } = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }) // Set force to true if you want to drop and recreate tables on every server start
+  .then(() => {
+    console.log('Sequelize models synced to the database');
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing Sequelize models:', error);
+  });
